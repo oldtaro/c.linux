b6e96603dbd59b90de1134e080b1789ce41efc2e
luo-zhenyu1234 <1184318227@qq.com>
2022-01-17 14:29:05 +0800

    上传

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world\n");
+    FILE *fp=fopen("noexist","rb");
+    if(NULL==fp)
+    {
+        perror("fopen");
+        return -1;
+    }
+    return 0;
+}
+
