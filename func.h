81d82cd46c94c1758e2a1fc5b19f17c9898825e6
luo-zhenyu1234 <1184318227@qq.com>
2022-01-20 23:27:53 +0800

    h

--- func.h
+++ func.h
@@ -10,6 +10,12 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <strings.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #define ARGS_CHECK(argc,num) {if(argc!=num) {printf("error args\n");return -1;}}
 #define ERROR_CHECK(ret,retval,func_name) {if(ret==retval) {perror(func_name);return -1;}}
