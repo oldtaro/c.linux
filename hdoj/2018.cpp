80e20a240ee5d2a11c4d17692f0e794ca1e47eea
luo-zhenyu1234 <1184318227@qq.com>
2022-02-13 21:40:48 +0800

    2018

--- hdoj/2018.cpp
+++ hdoj/2018.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 //母牛统计，当年牛数=去年牛数+可生牛数
 int main(void){
-    int i,n;
+    int n;
     int cow[56]={1,2,3};
-    for(i=3;i<56;i++){
-        cow[i]=cow[i-1]+cow[i-3];
+    for(n=3;n<56;n++){
+        cow[n]=cow[n-1]+cow[n-3];
     }
     while(scanf("%d",&n),n!=0){
         printf("%d\n",cow[n]);
