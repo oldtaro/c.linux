85b3a391401d7d4abdcbc7a237083bf6db3dc1c5
luo-zhenyu1234 <1184318227@qq.com>
2022-02-13 20:41:54 +0800

    2016

---
+++ hdoj/2016.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define swap(a,b) {int temp;temp=a;a=b;b=temp;}
+//交换一组数中最小的数和第一个元素的位置
+int main(void){
+    int arr[100]={0};
+    int n,min,i;
+    while(scanf("%d%d",&n,arr)!=EOF){
+        for(min=0,i=1;i<n;i++){
+            scanf("%d",arr+i);
+            if(arr[i]<arr[min]){
+                min=i;
+            }
+        }
+        swap(arr[0],arr[min]);
+        for(i=0;i<n;i++){
+            printf(i<(n-1)?"%d ":"%d\n",arr[i]);
+        }
+    }
+    return 0;
+}
